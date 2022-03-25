d4ac7faa1f29d622802d15de443a3f4ab5c3d0fd
rycont <rycont@outlook.kr>
2022-03-25 14:42:32 +0900

    feat: update changed structure

--- boilerplate/src/main.jsx
+++ boilerplate/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot override>
+      <RecoilNexus />
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
