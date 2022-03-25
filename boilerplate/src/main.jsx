cde16287ab84cd6798aaeb59d6c92504c35264c1
rycont <rycont@outlook.kr>
2022-03-25 15:29:01 +0900

    feat: add recoil dependency

--- boilerplate/src/main.jsx
+++ boilerplate/src/main.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
+import RecoilNexus from "recoil-nexus"
+
 import App from "./App";
+import "./style.css";
 
 ReactDOM.render(
   <React.StrictMode>
